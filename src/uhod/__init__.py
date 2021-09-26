909d91c8107c38b059cf667561ff770e5870da5f
ChangHoon Hahn <changhoon.hahn@princeton.edu>
2021-09-26 10:47:17 -0400

    setting up the directory

---
+++ src/uhod/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#__all__ = [""]
+
+__author__ = "ChangHoon Hahn"
+__email__ = "changhoon.hahn@princeton.edu"
+__uri__ = "https://github.com/changhoonhahn/arcoiris"
+__license__ = "MIT"
+__description__ = "Python Package for constructing a generative model of galaxy spectra"
+__copyright__ = "Copyright 2020 ChangHoon Hahn"
