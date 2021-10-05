a4245123ca650dc872e579e605d05331e661685b
changhoonhahn <changhoon.hahn@princeton.edu>
2021-10-05 10:50:33 -0400

    implemented i/o for AHF halos in della; LHC parameters also implemented

--- src/uhod/camels.py
+++ src/uhod/camels.py
@@ -5,6 +5,7 @@ module for interfacing with CAMELS data
 
 '''
 import os
+import numpy as np 
 import astropy.table as aTable 
 
 
@@ -12,7 +13,7 @@ def AHF(snap, real='LH_0', sim='tng'):
     ''' amiga halo finder given snapshot, realization, and simulation 
     '''
     # AHF directory
-    ahf_dir = os.path.join(dat_dir(real=real, sim=sim), 'AHF') 
+    ahf_dir = dat_dir(real=real, sim=sim)
     
     zsnap = z_snap(snap) 
     fsnap = 'snap%sRpep..0000.z%.3f.AHF_halos' % (str(snap).zfill(3), zsnap)
@@ -24,13 +25,23 @@ def AHF(snap, real='LH_0', sim='tng'):
     return tab
 
 
+def LHC(sim='tng'): 
+    ''' retrun Omega_m sigma_8 A1 A2 A3 A4 values of latin hyper cube
+    '''
+    _DAT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dat')
+    
+    return np.loadtxt(os.path.join(_DAT_DIR, '%s_lhc_params.txt' % sim)) 
+
+
 def dat_dir(real='LH_0', sim='tng'):
     ''' directory of CAMELS realization for given sim 
     '''
     dat_dirs = [
+            '/projects/QUIJOTE/CAMELS/AHF/',
             '/Users/chahah/data/CAMELS/Sims/' # mbp
             ]
-    sims = {'tng': 'IllustrisTNG'} 
+    sims = {'tng': 'IllustrisTNG', 
+            'simba': 'SIMBA'} 
 
     for _dir in dat_dirs: 
         dat_dir = os.path.join(_dir, sims[sim], real) 
